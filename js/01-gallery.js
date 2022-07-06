4f1b236d3bf3624304b5baf8f9eda2b7f2eab070
Lavelian <daniil.solodkyi@gmail.com>
2022-07-06 14:03:27 +0300

    final_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,9 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryEl = document.querySelector(".gallery");
-
 const galleryMarkup = createMarkupGallery(galleryItems);
+
 function createMarkupGallery(items) {
   return items
     .map(({ preview, original, description }) => {
@@ -22,6 +20,7 @@ function createMarkupGallery(items) {
     })
     .join(" ");
 }
+
 galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
 
 function OnCliclImage(event) {
@@ -29,18 +28,22 @@ function OnCliclImage(event) {
   if (!event.target.classList.contains("gallery__image")) {
     return;
   }
-  console.log(event.target);
 
   const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-`);
+     <img src="${event.target.dataset.source}" width="800" height="600" alt ="${event.target.alt}" >`);
 
   instance.show();
+
+  document.addEventListener(
+    "keydown",
+    (event) => {
+      if (event.code !== "Escape") {
+        return;
+      }
+      instance.close();
+    },
+    { once: true }
+  );
 }
+
 galleryEl.addEventListener("click", OnCliclImage);
-console.log(galleryEl);
