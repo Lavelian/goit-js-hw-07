8d698c5eafc6aa0f9bc690aa113644a4f1329125
Lavelian <daniil.solodkyi@gmail.com>
2022-07-06 12:58:03 +0300

    ///

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkup = createMarkupGallery(galleryItems);
+function createMarkupGallery(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join(" ");
+}
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function OnCliclImage(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  console.log(event.target);
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`);
+
+  instance.show();
+}
+galleryEl.addEventListener("click", OnCliclImage);
+console.log(galleryEl);
